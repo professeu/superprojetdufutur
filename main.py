84893e9904573e4e876d654e8d9448d5b7650bcd
maxime <failsafe@FRPARLPT0037609.cpgrp.root.local>
2024-03-03 23:12:26 +0100

    creation dun home scree , a modifier

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
 from game import Game
+from home_screen import Homescreen
 
 if __name__ == '__main__':
     pygame.init()
+    homescreen = Homescreen()
+    homescreen.run()
     game = Game()
     game.run()
 
